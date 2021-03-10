b9a1bbf14f83c9ce9e53e390f6ab0413e4d7fd49
camillaskar <camonaskar@gmail.com>
2021-03-10 12:09:27 +0800

    magic 8 ball

--- index.js
+++ index.js
@@ -1,18 +1,24 @@
-(function containsClass(){
-    const doc = document.documentElement; 
-    console.log("good");
-//function containsClass() {
-    let el = doc.getElementsByClassName("column");
-console.log(" kk " + el);
+// (function containsClass(){
+//     const doc = document.documentElement; 
+//     console.log("good");
+// //function containsClass() {
+//     let el = doc.getElementsByClassName("column");
+// console.log(" kk " + el);
 
-    (el==null) ? console.log("bye") : console.log("hello");
-//    if (el == null) {
-//         console.log("bye");
-//     }
-//     else{
-//         console.log("hello");
-//     }
- // }
+//     (el==null) ? console.log("bye") : console.log("hello");
+// //    if (el == null) {
+// //         console.log("bye");
+// //     }
+// //     else{
+// //         console.log("hello");
+// //     }
+//  // }
+// })();
 
-  
-})();
+function string() {
+    var person = prompt("Please enter your question");
+    if (person != null) {
+      document.getElementById("demo").innerHTML =
+      "Hello " + person + "! How are you today?";
+    }
+  }
