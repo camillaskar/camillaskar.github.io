2707e45b2bbb9b7203818ccc2ba05d5cccd88272
camillaskar <camonaskar@gmail.com>
2021-03-16 01:45:40 +0800

    signup

---
+++ signup/signup.js
@@ -0,0 +1,36 @@
+(function(){
+
+  
+
+    (function(){
+
+        let inputname = document.getElementById("name");
+        let inputlast = document.getElementById("lastname");
+        let inputuser = document.getElementById("username");
+        let inputpassword = document.getElementById("password");
+        let inputconfirm = document.getElementById("confirm");
+        let signup = document.getElementById("signup");
+
+        signup.addEventListener('click', function() {
+            let success = document.getElementById("success");
+            if (!checkLength(inputname) && !checkLength(inputlast) && !checkLength(inputuser) && !checkLength(inputpassword) && !checkLength(inputconfirm)){
+                success.textContent = "You have successfully registered!"; 
+            } else {
+                success.textContent = " ";
+            }
+            });
+
+    })();
+    function checkLength(input){
+        let isZeroLength = input.value.length == 0;
+        if (isZeroLength){
+            input.style.border = "solid #FF0000";
+        } else {
+            input.style.border = "solid #babdbb" ;
+        }
+        return isZeroLength;
+        };
+
+
+
+})();
